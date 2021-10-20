b376741379127ed8408a716e0aff8daf3ad9dbe4
Administrator <1600169564@qq.com>
2021-10-20 18:53:02 +0800

    更新三

---
+++ test/src/main/java/com/ex/test/HelloTest.java
@@ -0,0 +1,19 @@
+package com.ex.test;
+
+public class HelloTest {
+
+    public static final String A = "123";
+    public static final String B;
+
+    static {
+        B = "456";
+        S = A + B;
+    }
+
+    static String S;
+
+
+    public static void main(String[] args){
+        System.out.println(S == "123456");
+    }
+}
